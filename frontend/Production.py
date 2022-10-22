93ff074ee8b7e75800f20b52df63d4497105bd18
chermanegoh <gohchermane2904@gmail.com>
2022-10-22 23:08:42 +0800

    update interative map

--- frontend/Production.py
+++ frontend/Production.py
@@ -3,20 +3,10 @@ import dash_leaflet.express as dlx
 from dash import Dash, html, dcc, Output, Input
 from dash_extensions.javascript import assign
 import pandas as pd
-
-##to put into dockerfile later
-##pip install dash
-##pip install dash-leaflet
-##pip install dash-extensions
-##pip install --upgrade protobuf==3.20.0
-
-# A few cities 
-cities = [dict(name="Orchard", lat=1.306440046857804, lon=103.83163997540888),
-          dict(name="Ang Mo Kio", lat=1.3704522660724796, lon=103.84382793300924),
-          dict(name="Punggol", lat=1.3983072367008103, lon=103.90840283165096)]
+import plotly.express as px
 
 # get camera locations
-df = pd.read_csv(r'C:\Users\Chermane Goh\OneDrive - National University of Singapore\Y3S1\git\dsa3101-2210-09-lta\model\camera_data\all_cameras_ids_locations.csv')
+df = pd.read_csv(r'C:\Users\Chermane Goh\OneDrive - National University of Singapore\Y3S1\git\dsa3101-2210-09-lta\frontend\traffic_image_region.csv')
 camera = []
 for i in range(len(df)):
     d=dict(name = str(df.iloc[i,0]), lat = df.iloc[i,1], lon = df.iloc[i,2])
@@ -27,45 +17,90 @@ dd_options = [dict(value=c["name"], label=c["name"]) for c in camera]
 dd_defaults = [o["value"] for o in dd_options]
 # Generate geojson with a marker for each city and name as tooltip.
 geojson = dlx.dicts_to_geojson([{**c, **dict(tooltip=c['name'])} for c in camera])
-# Create javascript function that filters on feature name.
-geojson_filter = assign("function(feature, context){return context.props.hideout.includes(feature.properties.name);}")
-# Create example app.
+
+region = list(df['Region'].unique())
+cameraID = list(df['CameraID'].unique())
+
+# create line plot
+##line_fig = px.line(
+##    ##df,
+##    x='time',
+##    y='count')
+
 app = Dash()
 app.layout = html.Div([
     
+    html.Div(
+        children=[
+            html.H2('The following section will provide a breakdown of traffic situation at the camera location, please select the region and camera.'),
+            html.Br(),
+            html.H3('Select a Region'),
+            dcc.Dropdown(
+                id='region_dd',
+                options=[{'label':r, 'value':r} for r in region],
+                style={'width':'200px', 'margin':'0 auto'}),
+            html.Br(),
+            html.H3('Select a Camera'),
+            dcc.Dropdown(
+                id='camera_dd',
+                optionHeight=30,
+                maxHeight=150,
+                style={'width':'200px', 'margin':'0 auto'}),
+            html.Br(),
+        ],
+        
+        style={'text-align':'center', 'vertical-align':'top', 'background-color':'rgb(205,221,233)', 'padding':'30px'}
+    ),
+
     dl.Map(
         children=[
-        dl.TileLayer(),
-        dl.GeoJSON(data=geojson, options=dict(filter=geojson_filter), hideout=dd_defaults, id="geojson", zoomToBounds=True)
+            dl.TileLayer(),
+            dl.GeoJSON(data=geojson, id="geojson", zoomToBounds=True)
         ],
         style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}, id="map"),
-    
-    html.Div(children=[
-        html.Span("Select a region:"),
-        dcc.Dropdown(
-            id='title_dd',
-            options=[{'label':'North', 'value':'North'},
-                     {'label':'South', 'value':'South'},
-                     {'label':'East', 'value':'East'},
-                     {'label':'West', 'value':'West'}]),
-    ]),
 
-    html.Div(children=[
-        html.Span("Select a camera id:"),
-        dcc.Dropdown(id="dd", value=dd_defaults, options=dd_options, clearable=True),
-    ]),
-    
+    ##figure=line_plot
+    html.Div([dcc.Graph(id='lineplot')])
+
 ])
 
-# Link drop down to geojson hideout prop (could be done with a normal callback, but clientside is more performant).
-#app.clientside_callback("function(x){return x;}", Output("geojson", "hideout"), Input("dd", "value"))
-
+# Create a callback from the Region dropdown to the CameraID Dropdown
 @app.callback(
-    Output("geojson", "hideout"),
-    Input("dd", "value")
-)
-def update_output(value):
-    return value
+    Output('camera_dd', 'options'),
+    Input('region_dd', 'value'))
+
+def update_camera_dd(region_dd):
+  
+    region_camera = df[['Region', 'CameraID']].drop_duplicates()
+    relevant_camera_options = region_camera[region_camera['Region'] == region_dd]['CameraID'].values.tolist()
+    
+    # Create and return formatted relevant options with the same label and value
+    formatted_relevant_camera_options = [{'label':x, 'value':x} for x in relevant_camera_options]
+    
+    return formatted_relevant_camera_options
+
+# Create a callback from the CameraID dropdown to the map
+@app.callback(
+    Output("map", "children"),
+    Input("camera_dd", "value"))
+
+def update_map(cam_id):
+    
+    map_title = 'All Cameras'
+    df_map = df.copy()
+    df_map = df_map[df_map['CameraID'] == cam_id]
+
+    cam = [dict(name = str(df_map.iloc[0,0]), lat = df_map.iloc[0,1], lon = df_map.iloc[0,2])]   
+    cam_geojson = dlx.dicts_to_geojson([{**c, **dict(tooltip=c['name'])} for c in cam])
+
+    new_map = dl.Map(
+                children=[
+                    dl.TileLayer(),
+                    dl.GeoJSON(data=cam_geojson, zoomToBounds=True)
+                ],
+                style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}, id="map")
+    
+    return new_map
 
 if __name__ == '__main__':
     app.run_server()
