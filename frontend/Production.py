1dbc8ed43fd75aaa21521481c0acf6ee5c02e9c6
ar-c50n <e0538125@u.nus.edu>
2022-10-23 15:41:42 +0800

    add time series plot

--- frontend/Production.py
+++ frontend/Production.py
@@ -1,3 +1,5 @@
+import io, base64, requests
+import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash import Dash, html, dcc, Output, Input
@@ -9,6 +11,7 @@ import plotly.express as px
 ##url = 'https://raw.githubusercontent.com/ReverseCache/dsa3101-2210-09-lta/main/frontend/traffic_image_region.csv?token=GHSAT0AAAAAABZPVYLWEQCR25P3XJOPIDYKY2UAHNQ'
 ##df = pd.read_csv(url, index_col=0)
 df = pd.read_csv(r'C:\Users\Chermane Goh\OneDrive - National University of Singapore\Y3S1\git\dsa3101-2210-09-lta\frontend\traffic_count_sample.csv')
+df2 = pd.read_csv('traffic_his_sample.csv')
 
 #scatter map plot showing count of cars across singapore
 fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Count", size="Count",
@@ -31,13 +34,8 @@ geojson = dlx.dicts_to_geojson([{**c, **dict(tooltip=c['name'])} for c in camera
 region = list(df['Region'].unique())
 cameraID = list(df['CameraID'].unique())
 
-# create line plot
-##line_fig = px.line(
-##    ##df,
-##    x='time',
-##    y='count')
 
-app = Dash()
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([
 
     html.Div(
@@ -77,10 +75,43 @@ app.layout = html.Div([
             dl.GeoJSON(data=geojson, id="geojson", zoomToBounds=True)
         ],
         style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}, id="map"),
+    
+    html.Div([
+        dcc.Graph(id='line_graph',
+        style={'background-color':'rgb(205,221,233)', 'padding':'30px'})
+    ]),
+    
+    dbc.Row(
+        [
+            dbc.Col(html.Div(
+            [dcc.Upload( 
+            id='upload-data', 
+            children=html.Div([ 'Drag and Drop or ', 
+            html.A('Select File') ], 
+            style={
+                'width': '96%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px' 
+                }), 
+            multiple=False
+            )],
 
-    ##figure=line_plot
-    html.Div([dcc.Graph(id='lineplot')])
+            style={'padding': '5px 10px',
+                   'background-color': 'SkyBlue'}), width=6),
 
+            dbc.Col(html.Div(id='upload-data-contents', 
+            style={'marginLeft': 'auto', 'marginRight':'auto', 
+                   'textAlign': 'center'}), 
+            width=6)
+        ],
+        style={'background-color':'rgb(205,221,233)', 'padding':'30px'}
+    ),
+                    
 ])
 
 # Create a callback from the Region dropdown to the CameraID Dropdown
@@ -121,9 +152,50 @@ def update_map(cam_id):
     
     return new_map
 
+# create line plot for past 30-min data
+@app.callback(
+    Output('line_graph', 'figure'), 
+    Input('region_dd', 'value'),
+    Input('camera_dd', 'value'))
+def display_plot(reg, cam_id):
+    ft1 = df2[df2.Region==reg]
+    ft2 = ft1[ft1.Id==cam_id]
+    fig = px.line(ft2, x='Time', y='Count',
+                  title='Past 30 minutes')
+    return fig
+
+# copy from dash_app.py from dash_demo folder, only work for csv file
+@app.callback(Output('upload-data-contents', 'children'),
+             Input('upload-data', 'contents'))
+def display_data(u_contents):
+    if u_contents is not None:
+        content_type, content_string = u_contents.split(',')
+
+        decoded = base64.b64decode(content_string)
+        in_df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        in_dict = in_df.to_dict('list')
+        print(in_dict)
+        r1 = requests.post(url1, json=in_dict)
+        in_df['predictions'] = pd.to_numeric(pd.Series(r1.json()))
+
+        return dash_table.DataTable(
+            in_df.to_dict('records'),
+            [{'name': i, 'id': i} for i in in_df.columns],
+            sort_action= 'native',
+            filter_action='native', page_size=2,
+            style_header={'background-color': 'DimGray',
+                          'color':'white'},
+            style_data_conditional=[
+            {
+            'if': {
+                'column_id': 'predictions',
+            },
+            'backgroundColor': 'SandyBrown',
+            'color': 'white'
+            }]
+        )
+
 if __name__ == '__main__':
-    app.run_server()
-
-
+    app.run_server(debug=True)
 
 
