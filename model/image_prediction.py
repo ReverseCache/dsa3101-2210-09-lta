89f3b7a1e87a551e3b3685992bf9547b98d1258f
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-25 02:14:46 +0800

    Update image_prediction.py

--- model/image_prediction.py
+++ model/image_prediction.py
@@ -9,7 +9,7 @@ from io import BytesIO
 from datetime import datetime
 # ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def get_prediction(image_link = None, real_time = True):
+def get_prediction(image_link = None):
     file_path = os.path.dirname(os.path.abspath(__file__))
     weights_path = file_path + "/weights/"
     curr_datetime = datetime.now().strftime('%Y-%m-%d %H.%M.%S')
@@ -27,9 +27,7 @@ def get_prediction(image_link = None, real_time = True):
     camera_ids = []
     images_datetime = []
 
-    if real_time:
-        assert image_link is None
-
+    if image_link is None:
         jsonObj = get_json()
         camera_info = jsonObj["value"]
         i = 0
@@ -48,14 +46,8 @@ def get_prediction(image_link = None, real_time = True):
                 images_datetime.append(image_datetime)
             except Exception as e:
                 print(str(e))
-            # break
-            # i += 1
-            # if i == 3:
-            #     break
 
     else:
-        assert image_link is not None
-
         images.append(image_link)
         camera_ids.append(image_link)
         images_datetime.append(image_link)
@@ -71,42 +63,12 @@ def get_prediction(image_link = None, real_time = True):
 
         PIL_images_names = list(map(lambda x: "image{}".format(x), range(len(camera_ids))))
         df = pd.DataFrame({"camera_id": camera_ids, "PIL_image_name": PIL_images_names,
-                        "images_datetime": image_datetime, "count": count_vehicles, "is_congested": congestions})
+                        "images_datetime": images_datetime, "count": count_vehicles, "is_congested": congestions})
         df.to_csv(df_output_path, index = False)
 
         count_results.save(save_dir = count_images_output_path)
         congestion_results.save(save_dir = congestion_images_output_path)
         
     
-
 if __name__ == "__main__":
     get_prediction()
-
-
-
-
-# for zipped_file in os.listdir(images_path):
-#     with zipfile.ZipFile(images_path + os.path.basename(zipped_file)) as zf:
-#         for image_name in zf.namelist():
-#             try:
-#                 image = Image.open(zf.open(image_name))
-#                 images.append(image)
-#                 images_names.append(image_name)
-#             except Exception as e:
-#                 with open("error.txt", "a") as f:
-#                     f.write("{} {} {}\n".format(zipped_file, image_name, str(e)))
-#             break
-#         break
-        
-
-
-# with open("success.txt", 'a') as f:
-#     for image_name in images_names:
-#         f.write("%s\n" % image_name)
-
-
-# results = model(images)
-
-# results.print()
-
-# results.save()
