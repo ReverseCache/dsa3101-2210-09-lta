4c62ba16acae987f9b9a5ac2407c990b05749824
S A <amruthsrivathsancode@gmail.com>
2022-10-26 16:40:31 +0800

    structring calls

---
+++ docker/FileServer/Fileserver.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+
+def FileServer():
+    PORT = 4321
+
+    httpd = socketserver.TCPServer(
+        ("", PORT), http.server.SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    FileServer()
