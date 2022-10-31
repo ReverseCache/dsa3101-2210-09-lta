d54abae4fce2af213f14c17e2752b43e91e1ea82
S A <amruthsrivathsancode@gmail.com>
2022-11-01 05:03:00 +0800

    fixed fileserver logic

--- docker/FileServer/Fileserver.py
+++ docker/FileServer/Fileserver.py
@@ -2,41 +2,46 @@ import http.server
 import socketserver
 import json
 import pika
+import time
 
 
 def FileServer():
     PORT = 4321
-
     httpd = socketserver.TCPServer(
         ("", PORT), http.server.SimpleHTTPRequestHandler)
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
-    FileServer()
 
-    credentials = pika.PlainCredentials("guest", "guest")
-
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters("rabbitmq", 5672, "/", credentials)
-    )
-    channel = connection.channel()
+    while True:
+        try:
+            credentials = pika.PlainCredentials("guest", "guest")
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters("rabbitmq", 5672, "/", credentials)
+            )
+            channel = connection.channel()
+            break
+        except Exception as e:
+            print("Waiting for connection")
+            time.sleep(5)
 
     channel.queue_declare(queue='ApiFileQ')
 
     def saveIncidentsBody(serialised_message):
-        with open('incidents.json', 'w', encoding='utf-8') as f:
-            json.dump(json.load(serialised_message),
-                      f, ensure_ascii=False, indent=4)
+        jSon = json.load(serialised_message)
+        json.dump(jSon, write_file, indent=2)
         print("Traffic Incidents saved to disk")
 
     def callbackIncidents(ch, method, properties, body):
-        print(" [x] Received %r" % body)
         try:
             saveIncidentsBody(body)
         except Exception as e:
-            print(e.message)
+            print("failed")
 
-    channel.basic_consume(queue='ApiFileQ', on_message_callback = callbackIncidents, auto_ack=True)
-
+    channel.basic_consume(
+        queue='ApiFileQ', on_message_callback=callbackIncidents, auto_ack=True)
+    print(" I AM CONSUMING")
     channel.start_consuming()
+    time.sleep(30)
+    FileServer()
