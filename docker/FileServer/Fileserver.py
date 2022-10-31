f24b9529fb650b00a2222af1584b180abc525607
S A <amruthsrivathsancode@gmail.com>
2022-10-31 23:40:32 +0800

    :)

--- docker/FileServer/Fileserver.py
+++ docker/FileServer/Fileserver.py
@@ -1,5 +1,7 @@
 import http.server
 import socketserver
+import json
+import pika
 
 
 def FileServer():
@@ -12,3 +14,28 @@ def FileServer():
 
 if __name__ == "__main__":
     FileServer()
+
+    credentials = pika.PlainCredentials("guest", "guest")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("rabbitmq", 5672, "/", credentials)
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue='ApiModelQ')
+
+    def saveIncidentsBody(serialised_message):
+        with open('incidents.json', 'w', encoding='utf-8') as f:
+            json.dump(json.load(serialised_message),
+                      f, ensure_ascii=False, indent=4)
+        print("Traffic Incidents saved to disk")
+
+    def callbackIncidents(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        try:
+            saveIncidentsBody(body)
+        except Exception as e:
+            print(e.message)
+
+    channel.basic_consume(callbackIncidents, queue='ApiModelQ', no_ack=True)
+    channel.start_consuming()
