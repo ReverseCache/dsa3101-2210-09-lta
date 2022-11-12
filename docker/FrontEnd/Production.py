df2ad5d60dd9e975f3e7ea1308fc2df82950ad0b
leejintehh <110539446+leejintehh@users.noreply.github.com>
2022-11-12 19:25:59 +0800

    for clifton

--- docker/FrontEnd/Production.py
+++ docker/FrontEnd/Production.py
@@ -32,10 +32,10 @@ while True:
 
 
 #scatter map plot showing count of cars across singapore
-fig = px.scatter_mapbox(latest_df, lat="latitude", lon="longitude", color="count", size="count",
-                        hover_data={'latitude':False, 'longitude': False, 'roadname':True, 'region':True, 'count':True},
-                        color_continuous_scale=px.colors.sequential.Reds, size_max=15, zoom=10)
-fig.update_layout(mapbox_style="open-street-map")
+# fig = px.scatter_mapbox(latest_df, lat="latitude", lon="longitude", color="count", size="count",
+#                         hover_data={'latitude':False, 'longitude': False, 'roadname':True, 'region':True, 'count':True},
+#                         color_continuous_scale=px.colors.sequential.Reds, size_max=15, zoom=10)
+# fig.update_layout(mapbox_style="open-street-map")
 
 
 # interactive map displaying single camera
@@ -71,7 +71,7 @@ app.layout = html.Div([
     html.Div(
         children=[
             html.H2('Real-time count of Cars'),
-            dcc.Graph(figure=fig, id='scatter_map',
+            dcc.Graph(id='scatter_map',
                       style={'width': '80%', 'height': '100%', 'display':'inline-block'}),
         ]
     ),
@@ -193,18 +193,21 @@ style={'text-align':'center', 'background-color':'#C9DEF5', 'padding':'30px'})
     Input("camera_dd", "value"))
 
 def update_scatter_map(cam_id):
-
     #get latest data
     main_df = pd.read_csv('Ltadump.csv')
     main_df['images_datetime']=main_df['images_datetime'].apply(lambda x:x.replace('.',':'))
     main_df['images_datetime']=pd.to_datetime(main_df['images_datetime'])
     latest_df=main_df.sort_values('images_datetime',ascending=False).groupby('camera_id').head(1)
+    fig = px.scatter_mapbox(latest_df , lat="latitude", lon="longitude", color="count", size="count",
+                    hover_data={'latitude':False, 'longitude': False, 'roadname':True, 'region':True, 'count':True},
+                    color_continuous_scale=px.colors.sequential.Reds, size_max=15, zoom=10)
+    fig.update_layout(mapbox_style="open-street-map")
 
-    if cam_id:
-        fig = px.scatter_mapbox(latest_df , lat="latitude", lon="longitude", color="count", size="count",
-                        hover_data={'latitude':False, 'longitude': False, 'roadname':True, 'region':True, 'count':True},
-                        color_continuous_scale=px.colors.sequential.Reds, size_max=15, zoom=10)
-        fig.update_layout(mapbox_style="open-street-map")
+    # if cam_id:
+    #     fig = px.scatter_mapbox(latest_df , lat="latitude", lon="longitude", color="count", size="count",
+    #                     hover_data={'latitude':False, 'longitude': False, 'roadname':True, 'region':True, 'count':True},
+    #                     color_continuous_scale=px.colors.sequential.Reds, size_max=15, zoom=10)
+    #     fig.update_layout(mapbox_style="open-street-map")
     
     return fig
 
