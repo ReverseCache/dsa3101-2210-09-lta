6fc71d573659cc835114bb5140ae40f85588df74
cliftonfelix <cliftonfelix12@yahoo.com>
2022-11-14 02:01:36 +0800

    Remove multiple lines in run.sh

--- docker/FrontEnd/run.sh
+++ docker/FrontEnd/run.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-exec python3 ./callInterface.py &
-exec python3 ./receiveInterface.py &
-exec python3 ./Production.py
+exec python3 ./callInterface.py & exec python3 ./receiveInterface.py & exec python3 ./Production.py
